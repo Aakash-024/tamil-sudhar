02103fd8cbf43cec2dbacebdb84673c05e93685c
Aakash Giridharan <aakashgiridharan1249@gmail.com>
2026-03-29 14:25:39 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "tamil-app-v99";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./Story 1.mp3",
+  "./Story 2.mp3",
+  "./Story 3.mp3",
+  "./Story 4.mp3",
+  "./Story 5.mp3",
+  "./Story 6.mp3",
+  "./Story 7.mp3",
+  "./Story 8.mp3",
+  "./Story 9.mp3",
+  "./Story 10.mp3",
+  "./Story 11.mp3",
+  "./Story 12.mp3",
+  "./Story 13.mp3",
+  "./Story 14.mp3",
+  "./Story 15.mp3"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
